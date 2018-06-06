b47993a276b99441b1cdfa20b4111fabc7c0cd27
Jesse Gabriel <jessetirayoh@gmail.com>
2018-06-05 21:12:42 -0700

    updated script.js in facebook app

---
+++ Facebook App/script.js
@@ -0,0 +1,23 @@
+var objUser = {
+    username: "Jesse",
+    password: "secret"
+};
+
+var database = [
+    objUser
+];
+
+var newsfeed = [
+    {
+        username: "David",
+        timeline: "What is going on?"
+    },
+    {
+        username: "Raymond",
+        timeline: "I'm still learning JavaScript"
+    },
+    {
+        username: "Gaby",
+        timeline: "JavaScript is dope"
+    }
+];
